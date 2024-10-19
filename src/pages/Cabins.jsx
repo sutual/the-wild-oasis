8fa841be928a01222024184678fe412990e82b94
ezira <ezratgab@gmail.com>
2024-10-19 22:30:21 +0300

    adding  condition to display form to create new cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,15 +1,23 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
 import CabinsTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import { useState } from "react";
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>TEST</p>
+        <p>Filter/Sort</p>
       </Row>
-      <Row type="horizontal">
+      <Row type="vertical">
         <CabinsTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add New Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
